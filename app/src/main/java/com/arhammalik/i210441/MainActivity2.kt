d06058663b9c0f6c0a4c0706d300c6d5405ca63d
Arham331 <69897147+Arham331@users.noreply.github.com>
2024-02-09 02:41:15 +0500

    2nd Commit
    In this Commit, i was able to add the functionality of LOGIN PAGE

--- app/src/main/java/com/arhammalik/i210441/MainActivity2.kt
+++ app/src/main/java/com/arhammalik/i210441/MainActivity2.kt
@@ -1,11 +1,49 @@
 package com.arhammalik.i210441
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.text.SpannableString
+import android.text.style.UnderlineSpan
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.arhammalik.i210441.R // Make sure to import the R file for your project
 
-class MainActivity2 : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity2 : AppCompatActivity()
+{
+    private lateinit var handler: Handler
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        //underline code
+        val textViewSignUp = findViewById<TextView>(R.id.textViewSignUp)
+        val content = SpannableString("Sign Up")
+        content.setSpan(UnderlineSpan(), 0, content.length, 0)
+        textViewSignUp.text = content
     }
+
+    fun forgotPasswordFunction(view: View)
+    {
+
+    }
+
+    fun loginFunction(view: View)
+    {
+
+    }
+
+    fun registerUserFunction(view: View)
+    {
+        handler = Handler()
+        handler.postDelayed(
+            {
+                val intent = Intent(this@MainActivity2, MainActivity3::class.java)
+                startActivity(intent)
+                finish()
+            }, 3000)
+    }
+
 }
