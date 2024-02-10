a8c564e6d6a47dcb9f95563f3a26d2c3f18ca229
Arham331 <69897147+Arham331@users.noreply.github.com>
2024-02-11 01:23:19 +0500

    Commit 6th
    5th Commit is for DASHBOARD, 6th Commit refers to ForgotPassword Page

--- app/src/main/java/com/arhammalik/i210441/MainActivity6.kt
+++ app/src/main/java/com/arhammalik/i210441/MainActivity6.kt
@@ -1,52 +1,45 @@
 package com.arhammalik.i210441
 
 import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.text.SpannableString
 import android.text.style.UnderlineSpan
 import android.view.View
 import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
-import com.arhammalik.i210441.R // Make sure to import the R file for your project
 
-class MainActivity2 : AppCompatActivity()
+class MainActivity6 : AppCompatActivity()
 {
     private lateinit var handler: Handler
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        setContentView(R.layout.activity_main6)
 
-        //underline code
-        val textViewSignUp = findViewById<TextView>(R.id.textViewSignUp)
-        val content = SpannableString("Sign Up")
+        val textViewSignUp = findViewById<TextView>(R.id.textViewLogin)
+        val content = SpannableString("Log In")
         content.setSpan(UnderlineSpan(), 0, content.length, 0)
         textViewSignUp.text = content
     }
 
-    fun forgotPasswordFunction(view: View)
+    fun BackToLoginPage(view: View)
     {
         handler = Handler()
         handler.postDelayed(
             {
-                val intent = Intent(this@MainActivity2, MainActivity6::class.java)
+                val intent = Intent(this@MainActivity6, MainActivity2::class.java)
                 startActivity(intent)
                 finish()
             }, 0)
     }
 
-    fun loginFunction(view: View)
-    {
-
-    }
-
-    fun registerUserFunction(view: View)
+    fun GoToRestPassword(view: View)
     {
         handler = Handler()
         handler.postDelayed(
             {
-                val intent = Intent(this@MainActivity2, MainActivity3::class.java)
+                val intent = Intent(this@MainActivity6, MainActivity7::class.java)
                 startActivity(intent)
                 finish()
             }, 0)
