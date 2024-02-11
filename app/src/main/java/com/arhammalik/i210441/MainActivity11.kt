7bc9819a5cf7f0d8ac3253310febe7378abb9b84
Arham331 <69897147+Arham331@users.noreply.github.com>
2024-02-12 02:52:30 +0500

    11th Commit
    Review Page Working Properly

--- app/src/main/java/com/arhammalik/i210441/MainActivity11.kt
+++ app/src/main/java/com/arhammalik/i210441/MainActivity11.kt
@@ -6,44 +6,34 @@ import android.os.Bundle
 import android.os.Handler
 import android.view.View
 
-class MainActivity10 : AppCompatActivity()
+class MainActivity11 : AppCompatActivity()
 {
     private lateinit var handler: Handler
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main11)
+    }
+
     fun GoToDashboard(view: View)
     {
         handler = Handler()
         handler.postDelayed(
             {
-                val intent = Intent(this@MainActivity10, MainActivity5::class.java)
+                val intent = Intent(this@MainActivity11, MainActivity5::class.java)
                 startActivity(intent)
                 finish()
             }, 0)
     }
 
-    fun GoToReviewPage(view: View)
+    fun GoToJohnCooper(view: View)
     {
         handler = Handler()
         handler.postDelayed(
             {
-                val intent = Intent(this@MainActivity10, MainActivity11::class.java)
+                val intent = Intent(this@MainActivity11, MainActivity10::class.java)
                 startActivity(intent)
                 finish()
             }, 0)
-    }
-
-    fun GoToCommunityPage(view: View)
-    {
-
-    }
-
-    fun GoToBookSessionPage(view: View)
-    {
-
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?)
-    {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main10)
     }
 }
