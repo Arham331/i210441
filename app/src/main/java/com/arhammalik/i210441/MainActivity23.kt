2dcebecb7e593f71b2cc6186c5ab230febdd66b7
Arham331 <69897147+Arham331@users.noreply.github.com>
2024-02-13 03:54:41 +0500

    Commit23
    Booked Sessions Page has been made

--- app/src/main/java/com/arhammalik/i210441/MainActivity23.kt
+++ app/src/main/java/com/arhammalik/i210441/MainActivity23.kt
@@ -1,10 +1,25 @@
 package com.arhammalik.i210441
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
 
 class MainActivity23 : AppCompatActivity()
 {
+    private lateinit var handler: Handler
+
+    fun GoToProfile(view: View)
+    {
+        handler = Handler()
+        handler.postDelayed(
+            {
+                val intent = Intent(this@MainActivity23, MainActivity21::class.java)
+                startActivity(intent)
+                finish()
+            }, 0)
+    }
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
