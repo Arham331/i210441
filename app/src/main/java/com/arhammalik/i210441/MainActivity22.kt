94cff8b14b1363c64de5d6344380abd297de7343
Arham331 <69897147+Arham331@users.noreply.github.com>
2024-02-13 03:22:55 +0500

    Commit 22
    Edit Profile Page is up and is fully functional

--- app/src/main/java/com/arhammalik/i210441/MainActivity22.kt
+++ app/src/main/java/com/arhammalik/i210441/MainActivity22.kt
@@ -1,10 +1,28 @@
 package com.arhammalik.i210441
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
 
-class MainActivity22 : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity22 : AppCompatActivity()
+{
+    private lateinit var handler: Handler
+
+    fun GoToProfile(view: View)
+    {
+        handler = Handler()
+        handler.postDelayed(
+            {
+                val intent = Intent(this@MainActivity22, MainActivity21::class.java)
+                startActivity(intent)
+                finish()
+            }, 0)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main22)
     }
