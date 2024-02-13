35b42e8426a5844e4249e315368b17a07d7cb2f2
Arham331 <69897147+Arham331@users.noreply.github.com>
2024-02-13 21:12:54 +0500

    commit 19
    Video Call functionally is fully working
    
    NOTE: commit 18 was about VIDEO not video call

--- app/src/main/java/com/arhammalik/i210441/MainActivity19.kt
+++ app/src/main/java/com/arhammalik/i210441/MainActivity19.kt
@@ -1,12 +1,26 @@
 package com.arhammalik.i210441
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.View
 
 class MainActivity19 : AppCompatActivity()
 {
     private lateinit var handler: Handler
+
+    fun GoToJohnCooperChat(view: View)
+    {
+        handler = Handler()
+        handler.postDelayed(
+            {
+                val intent = Intent(this@MainActivity19, MainActivity13::class.java)
+                startActivity(intent)
+                finish()
+            }, 0)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
